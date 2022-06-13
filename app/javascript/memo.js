7d50df2c9927c2f81bc43722c8246b78215696ba
mavel3215 <105359814+mavel3215@users.noreply.github.com>
2022-06-13 21:13:39 +0900

    非公開の値管理

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -20,9 +20,9 @@ function post (){
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
-    
     XHR.responseType = "json";
     XHR.send(formData);
+
     XHR.onload = () => {
       if (XHR.status !=200){
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
